b69a72778f4762722e5d890516dd889f4049a9ea
Sasacoriror <turksacir@hotmail.com>
2025-08-14 16:07:34 +0200

    Added the Revenue and profitability data from the API in the financialDTO.java.

--- src/main/java/com/example/stocks/DTO/financialDTO.java
+++ src/main/java/com/example/stocks/DTO/financialDTO.java
@@ -11,6 +11,9 @@ public class financialDTO {
     @JsonProperty("fiscal_period")
     private String fiscalPeriod;
 
+    @JsonProperty("fiscal_year")
+    private String fiscalYear;
+
     @JsonProperty("tickers")
     private List<String> tickers;
 
@@ -41,6 +44,15 @@ public class financialDTO {
         @JsonProperty("revenues")
         private Revenues revenues;
 
+        @JsonProperty("net_income_loss")
+        private Net_Income_Loss net_income_loss;
+
+        @JsonProperty("gross_profit")
+        private Gross_Profit gross_Profit;
+
+        @JsonProperty("operating_income_loss")
+        private Operating_Income_Loss operating_income_loss;
+
         public Revenues getRevenues() {
             return revenues;
         }
@@ -48,11 +60,113 @@ public class financialDTO {
         public void setNRevenues(Revenues revenues) {
             this.revenues = revenues;
         }
+
+        public Net_Income_Loss getNet_income_loss() {
+            return net_income_loss;
+        }
+
+        public void setNet_income_loss(Net_Income_Loss net_income_loss) {
+            this.net_income_loss = net_income_loss;
+        }
+
+        public Gross_Profit getGross_Profit() {
+            return gross_Profit;
+        }
+
+        public void setGross_Profit(Gross_Profit gross_Profit) {
+            this.gross_Profit = gross_Profit;
+        }
+
+        public Operating_Income_Loss getOperating_income_loss() {
+            return operating_income_loss;
+        }
+
+        public void setOperating_income_loss(Operating_Income_Loss operating_income_loss) {
+            this.operating_income_loss = operating_income_loss;
+        }
     }
 
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Revenues {
+
+        @JsonProperty("value")
+        private Double value;
+
+        @JsonProperty("unit")
+        private String unit;
+
+        public Double getValue() {
+            return value;
+        }
+
+        public String getUnit() {
+            return unit;
+        }
+
+        public void setUnit(String unit) {
+            this.unit = unit;
+        }
+
+        public void setValue(Double value) {
+            this.value = value;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Net_Income_Loss {
+
+        @JsonProperty("value")
+        private Double value;
+
+        @JsonProperty("unit")
+        private String unit;
+
+        public Double getValue() {
+            return value;
+        }
+
+        public String getUnit() {
+            return unit;
+        }
+
+        public void setUnit(String unit) {
+            this.unit = unit;
+        }
+
+        public void setValue(Double value) {
+            this.value = value;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Gross_Profit {
+
+        @JsonProperty("value")
+        private Double value;
+
+        @JsonProperty("unit")
+        private String unit;
+
+        public Double getValue() {
+            return value;
+        }
+
+        public String getUnit() {
+            return unit;
+        }
+
+        public void setUnit(String unit) {
+            this.unit = unit;
+        }
+
+        public void setValue(Double value) {
+            this.value = value;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Operating_Income_Loss {
 
         @JsonProperty("value")
         private Double value;

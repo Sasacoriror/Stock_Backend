ffd91acf224c18c5eb4956803fd9bee0830968f4
Sasacoriror <turksacir@hotmail.com>
2025-10-02 16:36:32 +0200

    Backend: Removed totalPrice, totalInvested, return and percentageReturn from the portfolio database and from doing any calculation on the backend and moved the calculations to be done on the frontend.
    Still need to clean the commented lines out.

--- src/main/java/com/example/stocks/Model/Stocks.java
+++ src/main/java/com/example/stocks/Model/Stocks.java
@@ -42,24 +42,17 @@ public class Stocks {
     @JsonProperty("totalDividend")
     private double totalDivided;
 
-    @JsonProperty("totalPrice")
-    private double totalPrice;
-
-    @JsonProperty("totalInvested")
-    private double totalInvested;
-
-    @JsonProperty("return")
-    private double returnValue;
-
-    @JsonProperty("percentageReturn")
-    private double percentageReturn;
+    //@JsonProperty("totalPrice")
+    //private double totalPrice;
 
     //@ManyToOne
     //@JoinColumn(name = "user_id")
     //private User user;
 
+    //double totalPrice
+
     public Stocks(String stockName, int stockPrice, int stockQuantity, String companyName,  double currentPrice,
-                  double dividend, double totalDivided, double totalPrice, double totalInvested, double returnValue, double percentageReturn) {
+                  double dividend, double totalDivided) {
         this.stockName = stockName;
         this.stockPrice = stockPrice;
         this.stockQuantity = stockQuantity;
@@ -67,10 +60,7 @@ public class Stocks {
         this.currentPrice = currentPrice;
         this.dividend = dividend;
         this.totalDivided = totalDivided;
-        this.totalPrice = totalPrice;
-        this.totalInvested = totalInvested;
-        this.returnValue = returnValue;
-        this.percentageReturn = percentageReturn;
+        //this.totalPrice = totalPrice;
     }
 
     public Stocks() {}

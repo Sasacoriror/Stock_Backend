246ab03050122e0d9109de33e021cf1c6611df6a
Sasacoriror <turksacir@hotmail.com>
2025-10-16 01:46:03 +0200

    Small changes to Watchlist.html and Watchlist.js. Now they show the market cap and instead of showing the number as a hole is says 3.5T or 2.8B

--- src/main/resources/static/Dividend_Tracker/Watchlist/Watchlist.js
+++ src/main/resources/static/Dividend_Tracker/Watchlist/Watchlist.js
@@ -18,12 +18,26 @@ function renderTable(watchList) {
     watchList.forEach((watchlist) => {
         const tr = document.createElement('tr');
 
-        ['stockTickerInn', 'companyName', 'latestPrice', 'dividendYield'].forEach(key => {
+        ['stockTickerInn', 'companyName', 'latestPrice', 'marcet_Cap', 'dividendYield'].forEach(key => {
             const td = document.createElement('td');
             let value = watchlist[key];
 
-            if (['latestPrice'].includes(key)){
-                value = `${parseFloat(value).toFixed(2)}$`;
+            if (['marcet_Cap'].includes(key)){
+                value = parseFloat(value);
+
+                if (key === 'marcet_Cap') {
+                   if (value >= 1e12) {
+                       value = `$${(value / 1e12).toFixed(2)}T`;
+                   } else if (value >= 1e9) {
+                       value = `$${(value / 1e9).toFixed(2)}B`;
+                   } else if (value >= 1e6) {
+                       value = `$${(value / 1e6).toFixed(2)}M`;
+                   } else if (value >= 1e3) {
+                      value = `$${(value / 1e3).toFixed(2)}K`;
+                   }
+                }
+            } else if(['latestPrice'].includes(key)){
+                value = `$${parseFloat(value).toFixed(2)}`;
             }
 
             if (['dividendYield'].includes(key)){

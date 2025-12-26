cc3befb4167ddd5a8f812a740f6390b0a0716445
Sasacoriror <turksacir@hotmail.com>
2025-12-26 16:05:38 +0100

    Shows now the coming dividends for each stock owned

--- src/main/java/com/example/stocks/Record/Dividends.java
+++ src/main/java/com/example/stocks/Record/Dividends.java
@@ -1,12 +1,15 @@
 package com.example.stocks.Record;
 
+import com.example.stocks.Model.DividendCalender;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
 
 public record Dividends(
         @JsonProperty("Annual_Dividend") double annualDividend,
         @JsonProperty("Monthly_Dividend") double monthlyDividend,
         @JsonProperty("Days_Dividend") double dailyDividend,
         @JsonProperty("Hourly_Dividend") double hourlyDividend,
-        @JsonProperty("Yield_On_Cost") double yieldOnCost
-) {
-}
+        @JsonProperty("Yield_On_Cost") double yieldOnCost,
+        @JsonProperty("Dividend_Payments")List<DividendCalender> dividendCalenders
+) {}
